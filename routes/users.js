d8f3a4310c79edb7938a4643268a0a9457c6fc9c
frenlee <frenlee710@gmail.com>
2017-08-17 11:01:50 +0800

    emit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    eventEmitter.emit("ev1");
+    res.send('respond with a resource');
 });
+
+eventEmitter.on("ev1", eventFunction);
+
+function eventFunction() {
+    console.info("chufa----tgg--");
+}
 
 module.exports = router;
