2f67977a73ea0519ce32e3dc4ec1757300e8c96b
frankerzeng <frankerzeng@163.com>
2017-08-25 10:19:03 +0800

    emit

--- routes/demo1.js
+++ routes/demo1.js
@@ -1,12 +1,61 @@
 var express = require('express');
 var router = express.Router();
 var events = require('events');
+var querystring = require('querystring');
+var util = require("util");
+
+var postHTML = '<html><head><meta charset="utf-8"><title>菜鸟教程 Node.js 实例</title></head><body><form method="post">' +
+    '网站名：<input name="name"><br>网站 URL： <input name="url"><br><input type="submit"></form></body></html>';
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    var body = '';
+    req.on('data', function (da) {
+        body += da;
+    });
+    req.on('end', function () {
+        // 解析参数
+        body = querystring.parse(body);
+        // 设置响应头部信息及编码
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+
+        if (body.name && body.url) { // 输出提交的数据
+            res.write("网站名：" + body.name);
+            res.write("<br>");
+            res.write("网站 URL：" + body.url);
+        } else {  // 输出表单
+            res.write(postHTML);
+        }
+        res.end();
+    });
 });
 
+router.post('/', function (req, res, next) {
+    console.log('-------');
 
-module.exports = demo1;
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+        console.info(util.inspect(body));
+    });
+    req.on('end', function () {
+        // 解析参数
+        body = querystring.parse(body);
+        console.log(body);
+
+        // 设置响应头部信息及编码
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+
+        if (body.name && body.url) { // 输出提交的数据
+            res.write("网站名：" + body.name);
+            res.write("<br>");
+            res.write("网站 URL：" + body.url);
+        } else {  // 输出表单
+            res.write(postHTML);
+        }
+        res.end();
+    });
+});
+
+module.exports = router;
 
