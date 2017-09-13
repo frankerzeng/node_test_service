bf078aba6d05ddd1aa41a9d36015c62e5b3900dd
frankerzeng <frankerzeng@163.com>
2017-09-13 16:59:37 +0800

    vue

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-    </ul>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h1>{{ msg }}</h1>
+        <h2>Essential Ldddinks</h2>
+        <ul>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your fdsfsdVue.js App'
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome to Your fdsfsdVue.js App'
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 100px;
+    }
 
 </style>
