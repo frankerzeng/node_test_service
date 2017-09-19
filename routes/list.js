9bed1cc2e77e67783ae92493ac040726b62cd512
frankerzeng <frankerzeng@163.com>
2017-09-19 16:09:50 +0800

    vue http

---
+++ routes/list.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var events = require('events');
+var url = require('url');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    var arg = url.parse(req.url, true).query;
+    console.log(arg);
+    var page = arg.page
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    res.end(JSON.stringify([{key: "title1111", value: "content11"}, {key: "title2222", value: "content2222"}]));
+});
+module.exports = router;
