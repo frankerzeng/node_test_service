f037d731d05064c234f696f9407f14130b54f574
frankerzeng <frankerzeng@163.com>
2017-09-28 11:34:42 +0800

    vue 自定义指令

---
+++ hello-vue/src/page/demo_directives.vue
@@ -0,0 +1,38 @@
+<template xmlns:v-zfl="http://www.w3.org/1999/xhtml">
+    <div id="y">
+        <div class="color" v-zfl:true.bg.ss="false">
+            首付就是两地分居胜利大街方式了的会计分录
+            <button class="btn btn-primary" @click="one = !one">
+                改变
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    Vue.directive('zfl', function (el, binding) {
+        let val = binding.value;            // 值
+        let modifiers = binding.modifiers;  //修饰符
+        let arg = binding.arg;              //参数
+
+        console.log("val:" + val);
+        for (var m in modifiers) {
+            console.log("modifiers:" + m);
+        }
+        console.log("arg:" + arg);
+    });
+    export default {
+        name: "one-y",
+        data () {
+            return {
+                one: false
+            };
+        },
+        methods: {},
+    };
+</script>
+
+
+<style lang="scss">
+</style>
