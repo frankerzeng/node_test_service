33b7f9907e4174982f4363718f3e4c4769e7dc81
frankerzeng <frankerzeng@163.com>
2017-09-28 11:20:36 +0800

    vue 计算属性

---
+++ hello-vue/src/page/demo_computed.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="ttt">
+        <table class="table">
+            <tr>
+                <td><input type="text" v-model.number="one"/></td>
+            </tr>
+            <tr>
+                <td><input type="text" v-model.number="two"/></td>
+            </tr>
+            <tr>
+                <td>{{add}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "one-ttt",
+        data () {
+            return {
+                one: 0,
+                two: 0
+            };
+        },
+        methods: {},
+        computed: {
+            add(){
+                return this.one + this.two;
+            }
+        }
+    };
+</script>
+
+
+<style lang="scss">
+</style>
